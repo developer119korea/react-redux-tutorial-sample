dfa47f5a770c3f10a5eaa79646b15b91dc7ff7b2
Youngwan Cho <developer119korea@gmail.com>
2021-12-07 17:28:36 +0900

    컴포넌트의 재사용성을 위해 컨테이너 컴포넌트를 만들어 리덕스와 상호 작용하도록 수정

---
+++ src/containers/AddNumber.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+/* eslint-disable no-extra-bind */
+import AddNumber from "../components/AddNumber";
+import React, { Component } from "react";
+import store from "../store";
+
+export default class extends Component {
+  render() {
+    return <AddNumber onClick={function (size) {
+      store.dispatch({ type: 'INCREMENT', size: size });
+
+    }.bind(this)}></AddNumber>
+  }
+}
